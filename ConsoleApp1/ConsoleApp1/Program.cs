585a8e7206558a5988b7a2a8c2fba1d5f15a14f9
TimDeveloper97 <timdeveloper97@gmail.com>
2022-04-21 16:32:54 +0700

    create project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = RandomToApply();
+            Console.WriteLine("Hello World!");
+        }
+        public static float GetApplyChance(int level)
+        {
+            return level;
+        }
+        public static bool RandomToApply()
+        {
+            return 0 <= GetApplyChance(10);
+        }
+    }
+}
